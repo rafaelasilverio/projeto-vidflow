bc8a3927589ab61830dee14a04c9440681d7ccff
Rafaela Silvério <rafaelapetelin@gmail.com>
2023-08-31 11:23:23 -0300

    aula_1_parte1

--- script.js
+++ script.js
@@ -29,3 +29,27 @@ async function buscarEMostrarVideos(){
 
 buscarEMostrarVideos();
 
+
+const barraDePesquisa = document.querySelector(".pesquisar__input");
+
+barraDePesquisa.addEventListener("input", filtrarPesquisa);
+
+function filtrarPesquisa(){
+    const videos = document.querySelectorAll(".videos__item");
+
+    if(barraDePesquisa.value != ""){
+        for(let video of videos){
+            let titulo = video.querySelector(".titulo-video").textContent.toLowerCase();
+            let valorFiltro = barraDePesquisa.value.toLowerCase();
+
+            if(!titulo.includes(valorFiltro)){
+                video.style.display = "none";
+            } else {
+                video.style.display = "block";
+            }
+
+        }
+    } else {
+        video.style.display = "block";
+    }
+}
